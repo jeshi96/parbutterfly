7a78caac992a780dab567beb39fa7b48d5af9e5a
Julian Shun <jshun@mit.edu>
2018-08-09 03:55:08 +0000

    removing duplicate vertices in random hypergraph generator

--- utils/randHypergraph.C
+++ utils/randHypergraph.C
@@ -39,15 +39,18 @@ struct edgeSecondCmp {
 };
 
 template <class intT>
-hyperedgeArray<intT> hyperedgeRandom(long n, long m, long cardinality) {
-  long numEdges = m*cardinality;
+struct nonNeg {bool operator() (edge<intT> e) {return (e.u != UINT_T_MAX && e.v != UINT_T_MAX);}};
+
+template <class intT>
+hyperedgeArray<intT> hyperedgeRandom(long nv, long nh, long cardinality) {
+  long numEdges = nh*cardinality;
   edge<intT> *VE = newA(edge<intT>,numEdges);
   edge<intT> *HE = newA(edge<intT>,numEdges);
-  {parallel_for(long i=0;i<m;i++) {
+  {parallel_for(long i=0;i<nh;i++) {
       for(long j=0;j<cardinality;j++) {
 	ulong offset = i*cardinality+j;
-	HE[offset] = edge<intT>(i,hashInt(offset)%n);
-	VE[offset] = edge<intT>(hashInt(offset)%n,i);
+	HE[offset] = edge<intT>(i,hashInt(offset)%nv);
+	VE[offset] = edge<intT>(hashInt(offset)%nv,i);
       }
       //need to remove duplicates
       quickSort(VE+i*cardinality,cardinality,edgeFirstCmp());
@@ -55,25 +58,33 @@ hyperedgeArray<intT> hyperedgeRandom(long n, long m, long cardinality) {
       intT curr = HE[i*cardinality].v;
       for(long j=1;j<cardinality;j++) {
 	ulong offset = i*cardinality+j;
-	if(HE[offset].v == curr) {HE[offset].v = -1; VE[offset].v = -1;}
+	if(HE[offset].v == curr) {HE[offset].v = UINT_T_MAX; VE[offset].u = UINT_T_MAX;}
 	else curr = HE[offset].v;
       }
-      //filter out -1's
     }}
-  return hyperedgeArray<intT>(VE,HE,n,m,numEdges,numEdges);
+  //filter out -1's
+  edge<intT> *HE2 = newA(edge<intT>,numEdges);
+  intT mh = sequence::filter(HE,HE2,numEdges,nonNeg<intT>());
+  free(HE);
+  edge<intT> *VE2 = newA(edge<intT>,numEdges);
+  intT mv = sequence::filter(VE,VE2,numEdges,nonNeg<intT>());
+  free(VE);
+  //cout << mv << " " << mh << endl;
+  return hyperedgeArray<intT>(VE2,HE2,nv,nh,mv,mh);
 }
 
 //Generates a symmetrized hypergraph with n vertices and m hyperedges,
-//each hyperedge containing c vertices.
+//each hyperedge containing c random vertices. Duplicate vertices in a
+//hyperedge are removed.
 int parallel_main(int argc, char* argv[]) {
-  commandLine P(argc,argv,"[-n <numvertices>] [-m <numhyperedges>] [-c <cardinality>] <outFile>");
+  commandLine P(argc,argv,"[-nv <numvertices>] [-nh <numhyperedges>] [-c <cardinality>] <outFile>");
   char* fname = P.getArgument(0);
   
-  long n = P.getOptionLongValue("-n",100);
-  long m = P.getOptionLongValue("-m", 10*n);  
+  long nv = P.getOptionLongValue("-nv",100);
+  long nh = P.getOptionLongValue("-nh", 10*nv);  
   long cardinality = P.getOptionLongValue("-c", 3);
 
-  hyperedgeArray<uintT> EA = hyperedgeRandom<uintT>(n, m, cardinality);
+  hyperedgeArray<uintT> EA = hyperedgeRandom<uintT>(nv, nh, cardinality);
   hypergraph<uintT> G = hypergraphFromHyperedges<uintT>(EA);
   EA.del();
   writeHypergraphToFile<uintT>(G, fname);
